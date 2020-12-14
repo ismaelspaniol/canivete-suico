0e825c612bd1bc02e121ae629d44e6ac5968eba8
ismael spaniol <ismaelspaniol@gmail.com>
2020-12-14 13:50:37 -0300

    heros completed

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,46 @@
+import { MessageService } from './../message.service';
+import { HEROES } from './../mock-heroes';
+import { Hero } from './../hero';
+import { HeroService } from '../hero.service';
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+  heroes: Hero[];
+
+  selectedHero: Hero;
+
+
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    var id = 0;
+    if (!name) { return; }
+    this.heroService.addHero({id, name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
+
+}
